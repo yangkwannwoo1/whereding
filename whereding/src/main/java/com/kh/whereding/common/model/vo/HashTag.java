b0562f77cd4940bf8fc584d2427ca0e2f00eeb7d
wj <lwjong99@gmail.com>
2023-10-21 23:46:41 +0900

    Feat : hashTag.java
    해시태그 객체생성

---
+++ whereding/src/main/java/com/kh/whereding/common/model/vo/HashTag.java
@@ -0,0 +1,18 @@
+package com.kh.whereding.common.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class HashTag {
+
+	private String productNo;
+	private String tagContent;
+}
