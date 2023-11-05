9ffa8e9d9f2bf10cdc3be7d8ec1cfdb7619a0dc1
wj <lwjong99@gmail.com>
2023-11-05 22:08:08 +0900

    Feat : Implement consulting
    예약 기능 완성

--- whereding/src/main/java/com/kh/whereding/member/model/vo/Consulting.java
+++ whereding/src/main/java/com/kh/whereding/member/model/vo/Consulting.java
@@ -22,13 +22,20 @@ public class Consulting {
 		START_TIME
 		END_TIME
 	 */
+	
 	private int csNo;				// 상담예약 번호
 	private String productNo;		// 제품번호
 	private int userNo;				// 회원
-	private String scContent;		// 상담내용
+	private String csContent;		// 상담내용
 	private String csDate;			// 상담일
 	private String startTime;		// 상담시작시간
 	private String endTime;			// 상담종료시간
+	
+	/* 원종추가 */
+	private String consultingTime; // 상담시간
+	private String unitTime;	   // 단위시간
+	private String openTime;	   // 가게 오픈 시간
+	private String closeTime;	   // 가게 마감 시간
 	
 	
 	
--- whereding/src/main/java/com/kh/whereding/reservationBasket/model/service/ReservationBasketServiceImpl.java
+++ whereding/src/main/java/com/kh/whereding/reservationBasket/model/service/ReservationBasketServiceImpl.java
@@ -1,11 +1,16 @@
 package com.kh.whereding.reservationBasket.model.service;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kh.whereding.member.model.vo.Consulting;
 import com.kh.whereding.reservationBasket.model.dao.ReservationBasketDao;
 import com.kh.whereding.reservationBasket.model.vo.ReservationBasket;
 
@@ -23,5 +28,36 @@ public class ReservationBasketServiceImpl implements ReservationBasketService {
 		return rDao.selectBasketList(sqlSession, userNo);
 	}
 
+	@Override
+	public ArrayList<Consulting> selectTimeInfo(HashMap<String, String> map) {
+		return rDao.selectTimeInfo(sqlSession, map);
+	}
+
+	@Override
+	public ArrayList<Consulting> selectReservationList(HashMap<String, String> map) {
+		return rDao.selectReservationList(sqlSession, map);
+	}
+
+	@Override
+	public int selectReservation(Consulting c) {
+		return rDao.selectReservation(sqlSession, c);
+	}
+
+	@Override
+	public int insertReservation(Consulting c) {
+		int result = rDao.insertReservation(sqlSession, c);
+		System.out.println("insert " + result);
+		
+		return result;
+	}
+	
+	@Override
+	public int deleteReservation(Consulting c) {
+		
+		int result = rDao.deleteReservation(sqlSession, c);
+		System.out.println("delete " + result);
+		
+		return result;
+	}
 	
 }
